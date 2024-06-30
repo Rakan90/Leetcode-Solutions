4168b49cba55d8961c80702b4390dcff447454ea
Rakan90 <152086679+Rakan90@users.noreply.github.com>
2024-06-30 03:28:41 +0300

    comment of easy solution

--- src/Easy/Length_of_last_word.java
+++ src/Easy/Length_of_last_word.java
@@ -1,6 +1,21 @@
 package Easy;
 
 public class Length_of_last_word {
+//    Example 1:
+//
+//    Input: s = "Hello World"
+//    Output: 5
+//    Explanation: The last word is "World" with length 5.
+//    Example 2:
+//
+//    Input: s = "   fly me   to   the moon  "
+//    Output: 4
+//    Explanation: The last word is "moon" with length 4.
+//    Example 3:
+//
+//    Input: s = "luffy is still joyboy"
+//    Output: 6
+//    Explanation: The last word is "joyboy" with length 6.
     static public int lengthOfLastWord(String s) {
         int index = s.length() -1;
         int i;
